80c055d9c1b295fd46c0a5f3ba6b2e9d953e288c
meheraz1100 <meheraz1100@gmail.com>
2024-11-19 22:18:38 +0600

    group methods

---
+++ $groupMethods.js
@@ -0,0 +1,19 @@
+db.test.aggregate([
+    // stage - 1
+    { $group: { 
+        _id: null,
+         totalSalary: { $sum: "$salary" },
+         maxSalary: { $max: "$salary"},
+         minSalary: { $min: "$salary" },
+         avgSalary: { $avg: "$salary" }
+    }},
+    // stage -2
+    {
+        $project: {
+            totalSalary: 1,
+            maxSalary: 1,
+            minSalary: 1,
+            averageSalary: "$avgSalary"
+        }
+    }
+])
---
+++ rangeInGroup.js
@@ -0,0 +1,21 @@
+db.test.aggregate([
+    // stage - 1
+    { $group: { 
+        _id: null,
+         totalSalary: { $sum: "$salary" },
+         maxSalary: { $max: "$salary"},
+         minSalary: { $min: "$salary" },
+         avgSalary: { $avg: "$salary" },
+         
+    }},
+    // stage -2
+    {
+        $project: {
+            totalSalary: 1,
+            maxSalary: 1,
+            minSalary: 1,
+            averageSalary: "$avgSalary",
+            rangeBetweenMaxAndMin: { $subtract: ["$maxSalary", "$minSalary"] }
+        }
+    }
+])
