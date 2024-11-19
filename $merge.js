17061b9ae5bbf364a133d889f74ce46c8dc57252
meheraz1100 <meheraz1100@gmail.com>
2024-11-19 09:06:26 +0600

    $merge

---
+++ $merge.js
@@ -0,0 +1,10 @@
+db.test.aggregate([
+    // stage -1 
+    { $match: {gender: "Male"}},
+    // stage - 2
+    { $addFields: {course: "level:-2", eduTech: "Programming Hero", monerMoto: "Moner Iccha"}},
+    // stage-3
+    // { $project: {course: 1, eduTech: 1}},
+    //stage-4
+    { $merge: "test"}
+])
