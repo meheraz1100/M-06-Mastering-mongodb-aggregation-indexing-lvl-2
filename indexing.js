76bf7963b04a97109bde1d3e93a70dabe590444b
meheraz1100 <meheraz1100@gmail.com>
2024-11-20 09:14:42 +0600

    indexing in mongodb

---
+++ indexing.js
@@ -0,0 +1,3 @@
+// db.getCollection("massive-data").createIndex({email : 1})
+
+// ata hocche indexing er syntex, indexing er maddhome query time komano jay. amra jokhon onek beshi dat niye kaj korbo shudhu matro tokhon e indexing implement korbo. echarao indexing korar karone memory te space nite hoy. jar fole memory dhire dhire full hote thake .
